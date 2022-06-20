181c1a255aa7aa5516d376af0ddd31e01d6f13e9
evgenok <popovevgen18042001@mail.ru>
2022-06-20 21:49:37 +0300

    added contacts

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ function scripts() {
    return src([
       'node_modules/jquery/dist/jquery.js',
       'node_modules/slick-carousel/slick/slick.js',
-      'node_modules//fancybox/dist/js/jquery.fancybox.js',
+      'node_modules/fancybox/dist/js/jquery.fancybox.js',
       // 'node_modules/rateyo/src/jquery.rateyo.js',
       // 'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
       // 'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
