661442a8154471ade70898dcf36e2c56da4d4494
Vitek3573 <vitya_fetisov@mail.ru>
2022-07-04 18:11:13 +0300

    Сделано

--- app/js/main.js
+++ app/js/main.js
@@ -63,7 +63,10 @@ $(function () {
    $('.video__slider-atlas').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
-      arrows: false,
+      infinite: true,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev-vedeo"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path style="fill: #166966" d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"/></svg></button>',
+      nextArrow: '<button type="button" class="slick-next-video"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path style="fill: #166966" d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"/></svg></button>'
    });
 
 
@@ -99,7 +102,7 @@ for (let elm of elements) {
 }
 
 $(document).ready(function ($) {
-   $('.footer__social-btn, .call__btn').click(function () {
+   $('.footer__social-btn, .request__btn-feedback').click(function () {
       $('.form__bg').fadeIn();
       return false;
    });
